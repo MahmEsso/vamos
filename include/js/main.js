a3ba81638458659f4a5db5a083955004bcbe246c
MahmEsso <mahmoudessonoutty@gmail.com>
2025-10-21 00:49:08 +0300

    omer edits

--- include/js/main.js
+++ include/js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
 		center:true,
 		autoplayHoverPause:true,
 		autoplay:true,
-		responsive:{0:{items:1,},600:{ items:2,nav:true,},1000:{items:3,nav:true,},1300:{items:3,nav:true,}}
+		responsive:{0:{items:2,},600:{ items:2,nav:true,},1000:{items:3,nav:true,},1300:{items:3,nav:true,}}
 	});
 });
 
